008caaefefb40e9544c57126281394ffd8eb3035
sistema.not <sistema.not@LAB5-16.sistemafieto.rede>
2025-10-07 21:32:16 -0300

    cadastro ok!

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -9,12 +9,12 @@ import model.Usuario;
 
 public class UsuarioDAO {
 
-	public void inserir(Usuario u) throws Exception {
+	public void inserir(Usuario usuario) throws Exception {
 		String sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
 		try (Connection conn = conexaoBD.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-			ps.setString(1, u.getnome());
-			ps.setString(2, u.getemail());
-			ps.setString(3, u.getsenha());
+			ps.setString(1, usuario.getnome());
+			ps.setString(2, usuario.getemail());
+			ps.setString(3, usuario.getsenha());
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
--- src/gui/controllerCadUsuario.java
+++ src/gui/controllerCadUsuario.java
@@ -1,29 +1,52 @@
 package gui;
 
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
+import javafx.scene.control.TextField;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import application.conexaoBD;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class controllerCadUsuario {
 	@FXML
-	private Button BtSair;
+    private Button BtSair;
 
-	@FXML
-	private Button btCadastrar;
+    @FXML
+    private Button btCadastrar;
 
-	@FXML
-	private TextField txtEmial;
+    @FXML
+    private TextField txtEmail;
 
-	@FXML
-	private TextField txtSenha;
+    @FXML
+    private TextField txtNome;
 
-	@FXML
-	private TextField txtnome;
-	
-	
+    @FXML
+    private TextField txtSenha;
 
-	
+	public void cadastro(){
+		//btCadastrar.setOnAction(event-> {
+			String email = txtEmail.getText();
+			String nome = txtNome.getText();
+			String senha = txtSenha.getText();
+			
+			String sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+
+		try (Connection conn = conexaoBD.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
+			ps.setString(1, nome);
+			ps.setString(2, email);
+			ps.setString(3, senha);
+			
+			System.out.println("deu certo!");
+			
+		}catch(Exception e ) {
+			e.printStackTrace();
+		}
+			
+			
+
+	//	});
+
+}
 
 }
